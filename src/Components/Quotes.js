a088fe0e48361973172341be32f0bf7837215205
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-05-03 23:50:17 -0600

    basic navigation working

---
+++ src/Components/Quotes.js
@@ -0,0 +1,14 @@
+import logo from "../Images/WSTQlogo.png"
+import '../SASS/Nav.sass';
+import {BrowserRouter as NavLink} from 'react-router-dom';
+
+function Quotes() {
+    return (
+        <div>
+            <h1>Quotes</h1>
+        </div>
+    );
+  }
+  
+  export default Quotes;
+  
