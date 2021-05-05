2d3c7468dc70a4098d96888549f8c77f01b1c1d6
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-05-05 12:51:28 -0600

    added Carbon Design System to React project, added other components

---
+++ src/Components/Quote.js
@@ -0,0 +1,15 @@
+import logo from "../Images/WSTQlogo.png"
+import '../SASS/Nav.sass';
+import {BrowserRouter as NavLink} from 'react-router-dom';
+import React, {useState} from 'react';
+
+function Quote() {
+    return (
+        <div>
+            <h1>Quote</h1>
+        </div>
+    );
+  }
+  
+  export default Quote;
+  
--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -1,11 +1,17 @@
 import logo from "../Images/WSTQlogo.png"
 import '../SASS/Nav.sass';
 import {BrowserRouter as NavLink} from 'react-router-dom';
+import Quote from './Quote'
+import React, {useState} from 'react';
 
 function Quotes() {
+    const [quotes, setQuotes] = useState([])
     return (
         <div>
             <h1>Quotes</h1>
+            <input type="text" value={quotes.author} onChange={e => setQuotes({...quotes, author: e.target.value})}/>
+            <input type="text" value={quotes.quote} onChange={e => setQuotes({...quotes, quote: e.target.value})}/>
+            <input type="text" value={quotes.url} onChange={e => setQuotes({...quotes, url: e.target.value})}/>
         </div>
     );
   }
