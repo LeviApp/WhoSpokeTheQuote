bfca7dbb0b33eaa38a80ca553bce634835d52355
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-05-11 21:16:47 -0600

    added window animation on quotes

--- src/Components/NewQuote.js
+++ src/Components/NewQuote.js
@@ -1,21 +1,32 @@
 import logo from "../Images/WSTQlogo.png"
-import '../SASS/Nav.scss';
+import '../SASS/NewQuote.scss';
 import {BrowserRouter as NavLink} from 'react-router-dom';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import Quote from './Quote'
 
 function NewQuote() {
     const [newQuote, setNewQuotes] = useState({author: '', quote: '', url: ''})
 
+    const [quotes, setQuotes] = useState([])
+    // useEffect(() => {
+    //     ne
+    // })
+    const addQuote = () => {
+        setQuotes([...quotes, newQuote])
+    }
     return (
         <div>
             <h1>New Quote</h1>
-            <input type="text" value={newQuote.author} onChange={e => setNewQuotes({...newQuote, author: e.target.value})}/>
-            <input type="text" value={newQuote.quote} onChange={e => setNewQuotes({...newQuote, quote: e.target.value})}/>
-            <input type="text" value={newQuote.url} onChange={e => setNewQuotes({...newQuote, url: e.target.value})}/>
-            <img src={newQuote.url}/>
-            <h2>{newQuote.author}</h2>
-            <p>{newQuote.quote}</p>
-
+            
+            <input placeholder="author" type="text" value={newQuote.author} onChange={e => setNewQuotes({...newQuote, author: e.target.value})}/>
+            <input placeholder="quote" type="text" value={newQuote.quote} onChange={e => setNewQuotes({...newQuote, quote: e.target.value})}/>
+            <input placeholder="url" type="text" value={newQuote.url} onChange={e => setNewQuotes({...newQuote, url: e.target.value})}/>
+            <button onClick={addQuote}>ADD</button>
+            <section>
+            {quotes.map( quote => {
+                return <Quote author={quote.author} quote={quote.quote} url={quote.url} />
+            })}
+            </section>
         </div>
     );
   }
