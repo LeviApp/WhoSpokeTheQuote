ff3894a6ce93b606ae088a8ea32e9af36edf95c8
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-05-14 16:55:22 -0600

    basic post working

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -6,20 +6,23 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 function Quotes() {
 
-    const [quotes, setQuotes] = useState({})
+    const [quotes, setQuotes] = useState([])
 
 
     useEffect(() => {
-        axios.get("https://quotesdjango.herokuapp.com/quotes/32")
+        axios.get("https://quotesdjango.herokuapp.com/quotes/")
         .then( res => setQuotes(res.data))
         .catch(err => console.log(err, 'error'))
-    }, [quotes])
+
+        return () => {
+            console.log('unmounted')
+        }
+    }, [])
     return (
         <div>
-            {/* {quotes.map( quote => {
+            {quotes.map( quote => {
                 return <Quote key={quote.id} author={quote.title} quote={quote.text_body} url={quote.img_url} />
-            })} */}
-            <h1>{quotes.title}</h1>
+            })}
         </div>
     );
   }
