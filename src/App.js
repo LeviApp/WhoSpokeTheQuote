7b0844dd64d8f9f40072b62eaa893e14ccc61815
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-05-25 13:43:56 -0600

    added buttons and button styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Nav'
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import NewQuote from './Components/NewQuote';
 import Quotes from './Components/Quotes';
 import MouseIf from './Components/MouseIf';
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Route exact path="/" render={(props) =>  <Redirect to="/quotes" />} />
       <Route exact path="/quotes" render={(props) =>  <Quotes />} />
       <Route exact path="/new" render={(props) =>  <NewQuote />} />
       <Route exact path="/quotes/:id" render={(props) =>  <SingleQuote />} />
